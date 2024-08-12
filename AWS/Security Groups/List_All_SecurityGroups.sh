9ceea521831bc0bce806665e9da5b7126d56f97a
Rafael <107967743+jrafa1607@users.noreply.github.com>
2024-08-12 11:42:12 -0300

    Update List_All_SecurityGroups.sh

--- AWS/Security Groups/List_All_SecurityGroups.sh
+++ AWS/Security Groups/List_All_SecurityGroups.sh
@@ -7,7 +7,7 @@ do
         for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
                 echo "==== Verificando Ec2 da conta: $conta"
-                cmd=$(aws ec2 describe-network-interfaces  --filters Name=addresses.private-ip-address,Values=172.22.209.98 --output text --region $regiao --profile $conta)
+                cmd=$(aws ec2 describe-network-interfaces  --filters Name=addresses.private-ip-address,Values=x.x.x.x --output text --region $regiao --profile $conta)
                 echo "$conta - $regiao - $cmd" | tee -a ListSG.txt
                 echo "================================="
         done
